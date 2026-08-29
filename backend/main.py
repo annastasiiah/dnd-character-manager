e18a0cc2699e4958732aecefa9fe2a463141c434
annastasiiah <anastasiahorpynyuk@gmail.com>
2026-08-29 17:55:44 +0000

    Implement user registration

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,13 @@
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, status, HTTPException
 from sqlalchemy.orm import Session
 from sqlalchemy import text
+
+from models.user import User
+from models.character import Character
+from models.race import Race
+
+from schemas.user import UserCreate, UserResponse
+from security import hash_password
 
 
 from database import get_db
@@ -11,4 +18,43 @@ app = FastAPI()
 def test_db(db: Session = Depends(get_db)):
     row = db.execute(text(('SELECT 1')))
     return {'result': row.scalar()}
+
+@app.post("/users/registration", 
+        response_model = UserResponse, 
+        status_code = status.HTTP_201_CREATED)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+
+    existing_user = db.query(User).filter(
+        User.email == user.email
+        ).first()
+
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="User with this email already exists"
+        )
+    
+    existing_nickname = db.query(User).filter(
+        User.nickname == user.nickname
+        ).first()
+    
+    if existing_nickname:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="User with this nickname already exists"
+        )
+    
+    hashed_password = hash_password(user.password)
+
+    new_user = User(
+        email = user.email,
+        nickname = user.nickname,
+        password_hash = hashed_password
+    )
+    
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
 
